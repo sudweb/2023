a852312e59e09b71f11e430056e06df10b10a9b9
mab <git@ma-b.com>
2023-03-09 15:05:15 +0100

    Add opengraph & Twitter meta tags for better social media preview

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import useSiteMetadata from '../hooks/useSiteMetadata';
+import socialCard from '../assets/social-card.png';
 
 const Title = React.forwardRef(({ value }, ref) => {
   const siteMetadata = useSiteMetadata();
@@ -13,6 +14,16 @@ const Title = React.forwardRef(({ value }, ref) => {
         content={`La conférence Web surtout Humaine revient !
 Après 3 ans d’absence, Sud Web revient et pose de nouveau ses valises dans la ville rose.`}
       />
+      <meta property="og:title" content={title} />
+      <meta property="og:image" content={socialCard} />
+      <meta property="og:image:width" content="942" />
+      <meta property="og:image:width" content="558" />
+      <meta property="og:image:type" content="image/png" />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@sudweb" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:image" content={socialCard} />
     </>
   );
 });
