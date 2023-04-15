ea5a77c52b8ccab1e245e65ed755a40e516da075
mab <git@ma-b.com>
2023-04-15 22:21:16 +0200

    Extract Event component

---
+++ src/components/Event.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+import { Box, Paper, Stack, Typography } from '@mui/material';
+import { GatsbyImage } from 'gatsby-plugin-image';
+
+import EventIcon from './EventIcon';
+import RenderAst from './RenderAst';
+
+const noop = () => {};
+
+const Event = ({
+  open = true,
+  onClick = noop,
+  event: { type, title, htmlAst },
+  authors = [],
+  sx = {},
+  ...props
+}) => (
+  <Paper
+    component="article"
+    sx={{
+      p: 3,
+      borderRadius: 2,
+      ...sx,
+    }}
+    {...props}
+  >
+    <Stack direction="row" spacing={1} alignItems="flex-start">
+      <EventIcon
+        type={type}
+        sx={{ verticalAlign: 'middle', mr: 1 }}
+        onClick={onClick}
+      />
+      <Typography variant="h2">
+        {title}
+      </Typography>
+    </Stack>
+
+    <Box
+      sx={{
+        mt: 3,
+        overflow: 'hidden',
+        transition: 'max-height 250ms ease',
+        maxHeight: open ? 450 : 0,
+      }}
+    >
+      <RenderAst hast={htmlAst} />
+    </Box>
+
+    <Box component="aside">
+      {authors.map(({ id, name, htmlAst: bioAst, picture }) => (
+        <Stack key={id} direction="row" spacing={3}>
+          {/* Avatar */}
+          {picture && (
+            <Box sx={{ flexShrink: 0 }}>
+              <GatsbyImage image={picture} alt="" />
+            </Box>
+          )}
+
+          <Box>
+            {/* Name */}
+            <Typography variant="h3">{name}</Typography>
+
+            {/* Bio */}
+            <RenderAst hast={bioAst} options={{ bodyVariant: 'body2' }} />
+          </Box>
+        </Stack>
+      ))}
+    </Box>
+  </Paper>
+);
+
+export default Event;
--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import {
-  Box,
   Unstable_Grid2 as Grid,
-  Paper,
-  Stack,
   Typography,
 } from '@mui/material';
-
-import { GatsbyImage } from 'gatsby-plugin-image';
 
 import useEvents from '../hooks/useEvents';
 import useSpeakers from '../hooks/useSpeakers';
 
 import BorderBox from './BorderBox';
-import RenderAst from './RenderAst';
-import EventIcon from './EventIcon';
 import TextButton from './TextButton';
+import Event from './Event';
 
 const Schedule = ({ sx = {}, ...props }) => {
   const [runtime, setRuntime] = React.useState(false);
@@ -74,7 +68,7 @@ const Schedule = ({ sx = {}, ...props }) => {
       )}
 
       <Grid container spacing={2} sx={{ mt: 2 }}>
-        {fridayEvents.map(({ id, type, htmlAst, title, authors }) => (
+        {fridayEvents.map(({ id, authors, ...event }) => (
           <Grid
             item
             sm={showDetails.includes(id) ? 12 : 6}
@@ -85,50 +79,13 @@ const Schedule = ({ sx = {}, ...props }) => {
               transition: 'width 250ms ease',
             }}
           >
-            <Paper component="article" sx={{ p: 3, borderRadius: 2, flexGrow: 1 }}>
-              <Stack direction="row" spacing={1} alignItems="flex-start">
-                <EventIcon
-                  type={type}
-                  sx={{ verticalAlign: 'middle', mr: 1 }}
-                  onClick={() => toggle(id)}
-                />
-                <Typography variant="h2">
-                  {title}
-                </Typography>
-              </Stack>
-
-              <Box
-                sx={{
-                  mt: 3,
-                  overflow: 'hidden',
-                  transition: 'max-height 250ms ease',
-                  maxHeight: showDetails.includes(id) ? 450 : 0,
-                }}
-              >
-                <RenderAst hast={htmlAst} />
-              </Box>
-
-              <Box component="aside">
-                {authors.map(author => (
-                  <Stack key={author} direction="row" spacing={3}>
-                    {speakers[author].picture && (
-                      <Box sx={{ flexShrink: 0 }}>
-                        {/* Avatar */}
-                        <GatsbyImage image={speakers[author].picture} alt="" />
-                      </Box>
-                    )}
-
-                    <Box>
-                      {/* Name */}
-                      <Typography variant="h3">{speakers[author].name}</Typography>
-
-                      {/* Bio */}
-                      <RenderAst hast={speakers[author].htmlAst} options={{ bodyVariant: 'body2' }} />
-                    </Box>
-                  </Stack>
-                ))}
-              </Box>
-            </Paper>
+            <Event
+              open={showDetails.includes(id)}
+              onClick={() => toggle(id)}
+              event={event}
+              authors={authors.map(author => speakers[author])}
+              sx={{ flexGrow: 1 }}
+            />
           </Grid>
         ))}
       </Grid>
